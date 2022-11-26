1fcb42010b357e59e78353bb96e6c7eab267d4b8
d.ragozin <dmtryragozin@gmail.com>
2022-11-26 20:35:04 +0300

    [-] лишнее

--- src/utils/fakeRequest.ts
+++
@@ -1,6 +0,0 @@
-const fakeRequest = async <T>(fakeData?: T, timeout = 500): Promise<T | null> =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve(fakeData ?? null), timeout)
-  );
-
-export default fakeRequest;
